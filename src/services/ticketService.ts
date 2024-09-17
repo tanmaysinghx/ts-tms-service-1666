bb74e76e8419882a72d462c350736c9bff80ab01
Tanmay Singh <tanmaysinghx@gmail.com>
2024-09-18 00:09:07 +0530

    Added kafka Implemetation - producer level

--- src/services/ticketService.ts
+++ src/services/ticketService.ts
@@ -1,13 +1,16 @@
 import { PrismaClient, Ticket } from '@prisma/client';
+import kafka from 'kafka-node';
 
 const prisma = new PrismaClient();
+
+const client = new kafka.KafkaClient({ kafkaHost: 'localhost:9092' });
+const producer = new kafka.Producer(client);
 
 export const createTicket = async (data: Partial<Ticket>): Promise<Ticket> => {
     const { createdBy, title, description, category, priority, dueDate } = data;
     if (!createdBy || !title || !description || !priority) {
         throw new Error('Required fields: createdBy, title, description, and priority must be provided.');
     }
-
     const ticket = await prisma.ticket.create({
         data: {
             createdBy,
@@ -19,7 +22,20 @@ export const createTicket = async (data: Partial<Ticket>): Promise<Ticket> => {
             dueDate: dueDate ? new Date(dueDate) : null,
         },
     });
-
+    const kafkaMessage = {
+        ticketId: ticket.id,
+        title: ticket.title,
+        description: ticket.description,
+        createdBy: ticket.createdBy,
+        assignedTo: ticket?.assignedTo ?? null,
+        status: ticket.status,
+        priority: ticket.priority,
+        eventType: 'create-ticket'
+    };
+    producer.send([{ topic: 'ticket-updates', messages: JSON.stringify(kafkaMessage) }], (err, data) => {
+        if (err) console.error('Error sending Kafka message', err);
+        else console.log('Kafka message sent', data);
+    });
     return ticket;
 };
 
